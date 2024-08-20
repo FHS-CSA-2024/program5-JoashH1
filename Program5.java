96f5b05431646540188db9570c31c08746e42720
JoashH1 <hernandezjoash6@gmail.com>
2024-08-20 09:48:00 -0700

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -1,9 +1,35 @@
+
 //import stuff here?
 
 //Your code here
+class Program5{
+    public static void main(String[] args) {
+     int mRoyale = 286;
+     int gRoyale = 9;
+     int mKoopaking = 412;
+     int gKoopaking = 40;
+     int mPipeframe = 361;
+     int gPipeframe = 18;
+     int mBadwagon = 161;
+     int gBadwagon = 11;
+     
+     //Values
+     double royaleMPG = (double)mRoyale/gRoyale;
+     double koopaMPG = (double)mKoopaking/gKoopaking;
+     double frameMPG = (double)mPipeframe/gPipeframe;
+     double BadwagonMPG = (double)mBadwagon/gBadwagon;
+     
+     //Output values
+     System.out.println( "Mushroom Cup Prix Racer Average Miles/Per Gallon");
+     System.out.println( "Royale averaged " + royaleMPG +" m/g");
+     System.out.println( "Koopa King averaged " + koopaMPG +" m/g");
+     System.out.println( "Pipe Frame averaged " + frameMPG +" m/g");
+     System.out.println( "BadWagon averaged " + BadwagonMPG +" m/g");
 
-//Paste console output below:
-/*
 
-
-*/
+     
+     
+    
+        
+    }
+}
